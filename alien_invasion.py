bed64199df10f1ea470c14ff1156971a35b082cf
ChaoJiLiChen <33450388+ChaoJiLiChen@users.noreply.github.com>
2019-07-29 00:20:56 +0800

    创建设置类

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,12 @@
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
     """初始化游戏并创建一个屏幕对象"""
     pygame.init() #初始化pygame
-    screen = pygame.display.set_mode((1200,800)) #调用pygame类来创建一个屏幕对象并设置大小 之所以又两个括号是因为这是一个元组 而不简单是两个括号
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.secrrn_height)) #调用pygame类来创建一个屏幕对象并设置大小 之所以又两个括号是因为这是一个元组 而不简单是两个括号
     pygame.display.set_caption("Alien Invasion")#设置屏幕显示的名字
 
     #设置背景色
@@ -18,7 +20,7 @@ def run_game():
                 sys.exit()
 
         #每次循环都重新绘制屏幕
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
